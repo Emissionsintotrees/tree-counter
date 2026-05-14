50c2e31ea89098e06134556e620b98d61274ddcd
Carbon <mcmahonara@icloud.com>
2026-05-14 22:18:53 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'tree-counter-v1';
+const FILES = ['/', '/index.html'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(FILES))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(response => {
+      return caches.open(CACHE).then(cache => {
+        cache.put(e.request, response.clone());
+        return response;
+      });
+    }))
+  );
+});
